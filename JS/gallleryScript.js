def123830c93ae70670776e938508c8b33364e9c
Malcolm Davis <me@malcolmdavis.xyz>
2017-08-19 18:19:42 -0600

    Java script for the gallery added.

---
+++ JS/gallleryScript.js
@@ -0,0 +1,61 @@
+var imgadr= ['images/Activity-1.jpg','images/Activity-2.jpg','images/Activity-3.jpg','images/Activity-4.jpg','images/Activity-5.jpg'];
+var headArray= ['First Heading','Second Heading', 'Third Heading', '4th Heading','Last heading'];
+var paraArray= ['First paragraph is awesome!',
+				'Second paragraph goes here!', 
+				'Third paragraph for you',
+        '4th paragraph for you',
+				'Last but not least important paragraph!'];
+var cnt= 0;
+var timer;
+//gradient
+//var slider= document.getElementById('slider');??  add script at the end of html?
+// onload body ----or---- call the function at the beginning of script?-> script must be top or buttom?  
+function sliderTimer()
+{
+  timer= setInterval(slider, 5000);
+}
+
+function slider()
+{
+  cnt++;
+  if(cnt == imgadr.length)
+    cnt= 0;
+
+  var slider= document.getElementById('slider');
+  slider.style.backgroundImage= "url(\'"+imgadr[cnt]+"\')";
+  document.getElementById('sliderHeader').innerHTML= headArray[cnt];  //or append?
+  document.getElementById('sliderPara').innerHTML= paraArray[cnt];
+}
+
+function next()
+{
+  clearInterval(timer);
+  
+  cnt++; 
+  if(cnt == imgadr.length)
+    cnt= 0;
+    
+  var slider= document.getElementById('slider');
+  slider.style.backgroundImage= "url(\'"+imgadr[cnt]+"\')";
+  document.getElementById('sliderHeader').innerHTML= headArray[cnt];
+  document.getElementById('sliderPara').innerHTML= paraArray[cnt];
+  
+  sliderTimer();
+}
+
+function prev()
+{
+console.log('prev');
+  clearInterval(timer);
+  
+  cnt--;
+  if(cnt == -1)
+    cnt= imgadr.length-1;
+
+  var slider= document.getElementById('slider');
+  slider.style.backgroundImage= "url(\'"+imgadr[cnt]+"\')";
+  document.getElementById('sliderHeader').innerHTML= headArray[cnt];
+  document.getElementById('sliderPara').innerHTML= paraArray[cnt];
+    
+  sliderTimer();
+}
