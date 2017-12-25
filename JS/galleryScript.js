2815600b5b934663860767a14fc592ee558708ca
Malcolm Davis <me@malcolmdavis.xyz>
2017-12-24 19:22:25 -0600

    New activities added to gallery code.

--- JS/galleryScript.js
+++ JS/galleryScript.js
@@ -2,19 +2,23 @@ var imgadr= ['images/activities/Activity-1.jpg','images/activities/Activity-2.jp
             'images/activities/Activity-3.jpg','images/activities/Activity-4.jpg',
             'images/activities/Activity-5.jpg', 'images/activities/Activity-6.jpg',
             'images/activities/Activity-7.jpg','images/activities/Activity-8.jpg',
-            'images/activities/Activity-9.jpg'];
-var headArray= ['NASA Space Apps Challenge','Scale Reaching Program\'s Hackathon', 'NASA Space Poop Challenge',
+            'images/activities/Activity-9.jpg','images/activities/Activity-10.jpg',
+            'images/activities/Activity-11.jpg','images/activities/Activity-12.jpg'];
+var headArray= ['NASA Space Apps Challenge','IEEEXtreme 11.0', 'NASA Space Poop Challenge',
                 'CeNAT HPC WorkShops', 'GoTouch\'s Promote','GoTouch Mini Hackathon',
-                'Git WorkShop', 'Jocici', 'MPI WorkShop'];
+                'Git WorkShop', 'Jocici', 'MPI WorkShop', 'CeNAT Big Data WorkShops', 'How to Get a Job at Big 5 Lecture', 'Scale Reaching Program\'s Hackathon'];
 var paraArray= ['Mentored the 16 Costa Rican teams on subjects like software and hardware prototyping and business. This  is an international NASA organized hackathon that occurs over 48 hours in cities around the world, to address challenges we face on Earth and in space.',
-				'Lead a multidisciplinary team to develop a app/business idea to enhance the tour experience of the tour of Caminos de Osa Association. This through the use of the available resources.', 
+				'Help the organizers to make the IEEExtreme 11.0 in Costa Rica posible and Lead a Computer Science Team.', 
 				'Lead a multidisciplinary team to develop a solution on NASA\'s problem with the waste management for extended periods of time. The solution was planned but not implemented.',
         'Attended to a week long program of the National Center of High Technology on HPC subjects, specifically parallel programing.',
 				'Promote GoTouch iniciative in different activities, while learning different subjects in this activities.',
         'Started the development of a flexible app creator to inform about the activities of the Computer Investigation Center of the Costa Rica Institute of Technology.',
         'Elaborated and imparted a Git WorkShop for the Circuits and Systems Society of the IEEE TEC Branch.', 
         'Formed part of the Costa Rica Institute of Technology organization staff for the JOCICI(Costa Rican Conference on Computer and Computer Research).',
-        'Elaborated and imparted a MPI WorkShop for the Computer Society of the IEEE TEC Branch.'];
+        'Elaborated and imparted a MPI WorkShop for the Computer Society of the IEEE TEC Branch.',
+        'Attended to a week long program of the National Center of High Technology on HPC subjects, specifically Big Data.',
+        'Elaborated and imparted a How to Get a Job at Big 5 Lecture for the Computer Society of the IEEE TEC Branch.',
+        'Lead a multidisciplinary team to develop a app/business idea to enhance the tour experience of the tour of Caminos de Osa Association. This through the use of the available resources.'];
 var cnt= 0;
 var timer;
 //gradient
