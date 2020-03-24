2851ca62f9642ee29b8d87f00f6fd03eb4995952
Malcolm Davis <malcolm.davis.steele@intel.com>
2020-03-23 22:52:19 -0600

    Major GUI update
    - Add animations.
    - Reorganize profile section.
    - Remove blog link and page.
    - Remove old activities gallery and implement new one.
    - Project gallery spacing fixed.
    - Remove different style for mobile.
    - Resize skill section and remove proficiency level.

--- JS/menuScript.js
+++ JS/menuScript.js
@@ -1,26 +1,70 @@
 function scrollNav() {
-  $(document).ready(function(){
-  // Add smooth scrolling to all links
-  $("a").on('click', function(event) {
+  $(document).ready(function() {
+    // Add smooth scrolling to all links
+    $("a").on("click", function(event) {
+      // Make sure this.hash has a value before overriding default behavior
+      if (this.hash !== "") {
+        // Prevent default anchor click behavior
+        event.preventDefault();
 
-    // Make sure this.hash has a value before overriding default behavior
-    if (this.hash !== "") {
-      // Prevent default anchor click behavior
-      event.preventDefault();
+        // Store hash
+        var hash = this.hash;
 
-      // Store hash
-      var hash = this.hash;
-
-      // Using jQuery's animate() method to add smooth page scroll
-      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 800, function(){
-   
-        // Add hash (#) to URL when done scrolling (default click behavior)
-        window.location.hash = hash;
-      });
-    } // End if
+        // Using jQuery's animate() method to add smooth page scroll
+        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+        $("html, body").animate(
+          {
+            scrollTop: $(hash).offset().top
+          },
+          800,
+          function() {
+            // Add hash (#) to URL when done scrolling (default click behavior)
+            window.location.hash = hash;
+          }
+        );
+      } // End if
+    });
   });
-});
+}
+
+function menuColor() {
+  // Cache selectors
+  var lastId,
+    topMenu = $("#mainMenu"),
+    topMenuHeight = topMenu.outerHeight() + 1,
+    // All list items
+    menuItems = topMenu.find("a"),
+    // Anchors corresponding to menu items
+    scrollItems = menuItems.map(function() {
+      var item = $($(this).attr("href"));
+      if (item.length) {
+        return item;
+      }
+    });
+
+  // Bind to scroll
+  $(window).scroll(function() {
+    // Get container scroll position
+    var fromTop = $(this).scrollTop() + topMenuHeight;
+
+    // Get id of current scroll item
+    var cur = scrollItems.map(function() {
+      if ($(this).offset().top < fromTop) return this;
+    });
+    // Get the id of the current element
+    cur = cur[cur.length - 1];
+    var id = cur && cur.length ? cur[0].id : "";
+
+    if (lastId !== id) {
+      lastId = id;
+      // Set/remove active class
+      menuItems
+        .parent()
+        .removeClass("active")
+        .end()
+        .filter("[href=#" + id + "]")
+        .parent()
+        .addClass("active");
+    }
+  });
 }
